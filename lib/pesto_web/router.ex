346045c318e7c3505c8e990f1258abf912a7ebfa
hugostanley <stanleyhugo06@@gmail.com>
2024-06-07 00:32:19 +0800

    feat: add different layout and protect wrong live

--- lib/pesto_web/router.ex
+++ lib/pesto_web/router.ex
@@ -21,7 +21,6 @@ defmodule PestoWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
-    live "/guess", WrongLive
   end
 
   # Other scopes may use custom stacks.
@@ -66,9 +65,12 @@ defmodule PestoWeb.Router do
     pipe_through [:browser, :require_authenticated_user]
 
     live_session :require_authenticated_user,
+      layout: {PestoWeb.Layouts, :test},
       on_mount: [{PestoWeb.UserAuth, :ensure_authenticated}] do
       live "/users/settings", UserSettingsLive, :edit
       live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+
+      live "/guess", WrongLive
     end
   end
 
